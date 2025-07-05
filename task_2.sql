7ec73a494703e14cf9b4a8c1201134f36ddcaa10
lina <linabenaddi81@gmail.com>
2025-07-05 11:14:59 +0100

    Fix: uppercase table names for checker

--- task_2.sql
+++ task_2.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS Books (
     FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
 );
 
-CREATE TABLE IF NOT EXISTS Custumers (
+CREATE TABLE IF NOT EXISTS Custumors (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215),
